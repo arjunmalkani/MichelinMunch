383d39f129c0119d9c9c326a11c0f227fbe34c3a
frozehu <166183471+frozehu@users.noreply.github.com>
2025-04-17 15:15:10 -0400

    added code for insert method

--- cmake-build-debug/CMakeFiles/MichelinMunch.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/MichelinMunch.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/Admin/CLionProjects/MichelinMunch/src/main.cpp" "CMakeFiles/MichelinMunch.dir/src/main.cpp.obj" "gcc" "CMakeFiles/MichelinMunch.dir/src/main.cpp.obj.d"
+  "/Users/elyglez/Documents/GitHub/MichelinMunch/src/main.cpp" "CMakeFiles/MichelinMunch.dir/src/main.cpp.o" "gcc" "CMakeFiles/MichelinMunch.dir/src/main.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
