8a715591fcc9b1bc45dbd05033152e008a349d54
frozehu <166183471+frozehu@users.noreply.github.com>
2025-04-17 15:09:58 -0400

    updated insert method

--- include/HashMapChaining.h
+++ include/HashMapChaining.h
@@ -6,31 +6,43 @@ struct Node {
     string value;
     Node* next;
 
-    Node();
-    Node(int k, string v);
+    Node() : key(""), value(""), next(nullptr) {}
+    Node(string k, string v) : key(k), value(v) {}
 };
 
 class SeparateChaining {
     int capacity; // size of table
     int count; // num of items in table
-    int hashFunction(int key);
+    Node** table;
+    int hashFunction(string key);
     void reHashing();
     void resize();
 
     public:
-    SeparateChaining();
+    SeparateChaining() {
+        table = new Node*[capacity];
+        for (int i = 0; i < capacity; i++) {
+            table[i] = nullptr;
+        }
+    }
     ~SeparateChaining();
-    void insert(string key);
-    void deleteItem(string key);
+    void insert(string key, string value) {
+        int index = hashFunction(key);
+        Node *node = new Node(key, value);
+        node->next = table[index];
+        table[index] = node;
+        count++;
+        if (loadFactor() >= 0.75) {
+            reHashing();
+        }
+    }
+    // void deleteItem(string key, string value);
     string search();
     float loadFactor() const {
         return count/capacity;
     }
     int getSize() const { return capacity;}
     int getCount() const { return count;}
-
-
-
 
 
 };
