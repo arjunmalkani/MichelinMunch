2c8f4ef1ba5834f6cc88064f2b4e34272a95f670
frozehu <166183471+frozehu@users.noreply.github.com>
2025-04-19 15:30:37 -0400

    changed separate chaining class by adding template so i can nest hash maps, changed functionality in main for the updatemaps function

--- include/HashMapChaining.h
+++ include/HashMapChaining.h
@@ -1,42 +1,44 @@
 #include <iostream>
 using namespace std;
 
+template<typename Key, typename Value>
 struct Node {
-    string key;
-    string value;
+    Key key;
+    Value value;
     Node* next;
 
     Node() : key(""), value(""), next(nullptr) {}
-    Node(string k, string v) : key(k), value(v) {}
+    Node(Key k, Value v) : key(k), value(v) {}
 };
 
+template<typename Key, typename Value>
 class SeparateChaining {
     int capacity; // size of table
     int count; // num of items in table
-    Node** table;
-    int hashFunction(const string& key) {
-        return hash<string>{}(key) % capacity;
+    Node<Key, Value>** table;
+    int hashFunction(const Key& key) {
+        return hash<Key>{}(key) % capacity;
     }
     void reHash() {
         int oldCapacity = capacity;
         capacity *=2;
-        Node** oldTable = table;
-        table = new Node*[capacity];
+        Node<Key, Value>** oldTable = table;
+        table = new Node<Key, Value>*[capacity];
         for (int i = 0; i < capacity; i++) {
             table[i] = nullptr;
         }
         count = 0;
         for (int i = 0; i < oldCapacity; i++) {
-            Node* node = oldTable[i];
+            Node<Key, Value>* node = oldTable[i];
             while (node != nullptr) {
                 insert(node->key, node->value);
                 node = node->next;
             }
         }
         for (int i = 0; i < oldCapacity; i++) {
-            Node* curr = oldTable[i];
+            Node<Key, Value>* curr = oldTable[i];
             while(curr != nullptr) {
-                Node* node = curr;
+                Node<Key, Value>* node = curr;
                 curr = curr->next;
                 delete node;
             }
@@ -45,28 +47,71 @@ class SeparateChaining {
     }
 
     public:
+    SeparateChaining(const SeparateChaining& other) {
+        capacity = other.capacity;
+        count = other.count;
+        table = new Node<Key, Value>*[capacity];
+        for (int i = 0; i < capacity; i++) {
+            table[i] = nullptr;
+            Node<Key, Value>* curr = other.table[i];
+            Node<Key, Value>* prev = nullptr;
+            while (curr != nullptr) {
+                Node<Key, Value>* newNode = new Node<Key, Value>(curr->key, curr->value);
+                if (prev == nullptr) {
+                    table[i] = newNode;
+                } else {
+                    prev->next = newNode;
+                }
+                prev = newNode;
+                curr = curr->next;
+            }
+        }
+    }
+    SeparateChaining& operator=(const SeparateChaining& other) {
+        if (this == &other) { return *this;}
+        this->~SeparateChaining();
+        capacity = other.capacity;
+        count = other.count;
+        table = new Node<Key, Value>*[capacity];
+        for (int i = 0; i < capacity; i++) {
+            table[i] = nullptr;
+            Node<Key, Value>* curr = other.table[i];
+            Node<Key, Value>* prev = nullptr;
+            while (curr != nullptr) {
+                Node<Key, Value>* newNode = new Node<Key, Value>(curr->key, curr->value);
+                if (prev == nullptr) {
+                    table[i] = newNode;
+                } else {
+                    prev->next = newNode;
+                }
+                prev = newNode;
+                curr = curr->next;
+            }
+        }
+        return *this;
+    }
     SeparateChaining() {
         capacity = 10000;
         count = 0;
-        table = new Node*[capacity];
+        table = new Node<Key, Value>*[capacity];
         for (int i = 0; i < capacity; i++) {
             table[i] = nullptr;
         }
     }
     ~SeparateChaining() {
         for (int i = 0; i < capacity; i++) {
-            Node* curr = table[i];
+            Node<Key, Value>* curr = table[i];
             while(curr != nullptr) {
-                Node* node = curr;
+                Node<Key, Value>* node = curr;
                 curr = curr->next;
                 delete node;
             }
         }
         delete[] table;
     }
-    void insert(const string& key, const string &value) {
+    void insert(const Key& key, const Value &value) {
         int index = hashFunction(key);
-        Node *node = new Node(key, value);
+        auto *node = new Node<Key, Value>(key, value);
         node->next = table[index];
         table[index] = node;
         count++;
@@ -75,16 +120,27 @@ class SeparateChaining {
         }
     }
     // void deleteItem(string key, string value);
-    string search(string key) {
+    Value& search(Key key) {
         int index = hashFunction(key);
-        Node* curr = table[index];
+        Node<Key, Value>* curr = table[index];
         while(curr != nullptr) {
             if (curr->key == key) {
                 return curr->value;
             }
             curr = curr->next;
         }
-        return "Key not found";
+        throw runtime_error("Key not found");
+    }
+    bool contains(Key key) {
+        int index = hashFunction(key);
+        Node<Key, Value>* curr = table[index];
+        while(curr != nullptr) {
+            if (curr->key == key) {
+                return true;
+            }
+            curr = curr->next;
+        }
+        return false;
     }
     float loadFactor() const {
         return static_cast<float>(count)/capacity;
--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,83 @@
 #include <iostream>
 #include "../include/DatasetLoader.h"
-#include "HashMapChaining.h";
+#include "HashMapChaining.h"
 #include "HashMapOpen.h"
 
 void updateMaps(vector<Restaurant> &restaurants,
-    SeparateChaining &cityMapSc, SeparateChaining &starMapSc, SeparateChaining &moneyMapSc) {
+    SeparateChaining<string,
+    SeparateChaining<string,
+    SeparateChaining<string, string>>> &scMap) {
+
+    SeparateChaining<string, SeparateChaining<string, string>> cityMapSc;
+
+
     for (Restaurant &restaurant : restaurants) {
+        string city = restaurant.location;
         string name = restaurant.name;
-        cityMapSc.insert(name, restaurant.location);
-        starMapSc.insert(name, restaurant.starCount);
-        moneyMapSc.insert(name, restaurant.price);
+        string stars = restaurant.starCount;
+        string price = restaurant.price;
+
+
+        if (!scMap.contains(city)) {
+            scMap.insert(city, SeparateChaining<string, SeparateChaining<string, string>>());
+        }
+        auto& starMapSc = scMap.search(city);
+
+        if (!starMapSc.contains(stars)) {
+            starMapSc.insert(stars, SeparateChaining<string, string>());
+        }
+        auto& priceMapSc = starMapSc.search(stars);
+        priceMapSc.insert(price, name);
     }
 }
 int main() {
     // testing data parsing
     string filename = "../data/michelin_my_maps.csv";
     vector<Restaurant> restaurants = loadDataset(filename);
-    SeparateChaining cityMapSc, starMapSc, moneyMapSc;
-    updateMaps(restaurants, cityMapSc, starMapSc, moneyMapSc);
+
+    SeparateChaining<string,
+    SeparateChaining<string,
+    SeparateChaining<string, string>>> scMap;
+    updateMaps(restaurants, scMap);
 
     //testing maps
-    string restaurantName = "Le Bernardin";
-    cout << "City: " << cityMapSc.search(restaurantName) << endl;
-    cout << "Stars: " << starMapSc.search(restaurantName) << endl;
-    cout << "Price: " << moneyMapSc.search(restaurantName) << endl;
+    string city = "Miami, USA";
+    string stars = "2 Stars";
+    string price = "$$$";
+    if (scMap.contains(city)) {
+        auto starMap = scMap.search(city);
 
-    cout << "Total restaurants parsed: " << restaurants.size() << endl;
+        if (starMap.contains(stars)) {
+            auto priceMap = starMap.search(stars);
+
+            if (priceMap.contains(price)) {
+                string name = priceMap.search(price);
+                cout << "Restaurant found in " << city << " with " << stars << " and " << price << " price: " << name << endl;
+            } else {
+                cout << "No restaurant found at price: " << price << endl;
+            }
+        } else {
+            cout << "No restaurant found with " << stars << " in " << city << endl;
+        }
+    } else {
+        cout << "No restaurants found in city: " << city << endl;
+    }
+
+
+
+    //cout << "Total restaurants parsed: " << restaurants.size() << endl;
 
     // prints first 400 restaurants info to verify parsing works
-    for (int i = 0; i < 400; i++) {
-        const Restaurant& r = restaurants[i];
-        cout << "Name: " << r.name << endl;
-        cout << "Address: " << r.address << endl;
-        cout << "City: " << r.location  << endl;
-        cout << "Price: " << r.price << endl;
-        cout << "Cuisine: " << r.cuisine << endl;
-        cout << "Stars: " << r.starCount << endl;
-        cout << "------------------------" << endl;
-    }
+    // for (int i = 0; i < 10; i++) {
+    //     const Restaurant& r = restaurants[i];
+    //     cout << "Name: " << r.name << endl;
+    //     cout << "Address: " << r.address << endl;
+    //     cout << "City: " << r.location  << endl;
+    //     cout << "Price: " << r.price << endl;
+    //     cout << "Cuisine: " << r.cuisine << endl;
+    //     cout << "Stars: " << r.starCount << endl;
+    //     cout << "------------------------" << endl;
+    // }
 
     return 0;
 }
