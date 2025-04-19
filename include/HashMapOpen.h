3e9fbfe699d53d03b7f5917a60a105a3b62149a4
Anthony Perez <perezanthony1@ufl.edu>
2025-04-19 01:41:33 -0400

    Almost done hash open class

--- include/HashMapOpen.h
+++ include/HashMapOpen.h
@@ -17,13 +17,15 @@ private:
     vector<Bucket> table;
     int capacity;
     int size;
-    float loadFactor = 0.0f;
+    float loadFactor;
 
+    // uses std::hash to hash key values 7
     int hashing(const Key& key) const {
         return hash<Key>{}(key) & capacity;
     }
 
-    void rehash() {
+    // helper function for insert to resize table and rehash key values
+    void resize() {
         capacity *= 2;
         vector<Bucket> newTable;
         table.clear();
@@ -38,14 +40,35 @@ private:
     }
 
 public:
+    OpenAddressHashMap(int cap = 1000) : capacity(cap), size(0), loadFactor(0.75f) {
+        table.resize(capacity);
+    }
 
+    void insert(const Key& key, const Value& value) {
+        if((size/capacity) >= loadFactor) {
+            resize();
+        }
 
+        // gets index by getting key's hash value and doing mod capacity
+        int index = hashing(key) % capacity;
+        int i = 0;
 
+        // using quadratic probing to avoid clusters
+        while (i < capacity) {
+            // i quadractically increases for fitting probe location
+            int probe = (index + i * i) % capacity;
 
-
-    // core fields: Entry[], size, capacity, etc.
-    // insert(key, value)
-    // get(key)
-    // contains(key)
-    // rehash(), etc.
+            // if probed index is not occupied or the key is the same, place into index
+            if(!table[probe].occupied || table[probe].key == key) {
+                // bucket's key value pairs get set and it becomes occupied
+                table[probe].key = key;
+                table[probe].value = value;
+                table[probe].occupied = true;
+                size ++;
+                return;
+            }
+            // if table is occupied, i increases and loop repeats until found index
+            i++;
+        }
+    }
 };
