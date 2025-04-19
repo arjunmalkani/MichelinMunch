ef2ee760bd31a9169d65fb9fa8285e4ed9bc0020
malkarjun <145407983+malkarjun@users.noreply.github.com>
2025-04-18 23:05:03 -0400

    Added data parsing functionality and tested in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
-using namespace std;
+#include "../include/DatasetLoader.h"
 
-int main(){
-    cout << "Hello World." << endl;
+int main() {
+    // testing data parsing
+    string filename = "../data/michelin_my_maps.csv";
+    vector<Restaurant> restaurants = loadDataset(filename);
+
+    cout << "Total restaurants parsed: " << restaurants.size() << endl;
+
+    // prints first 400 restaurants info to verify parsing works
+    for (int i = 0; i < 400; i++) {
+        const Restaurant& r = restaurants[i];
+        cout << "Name: " << r.name << endl;
+        cout << "Address: " << r.address << endl;
+        cout << "City: " << r.location  << endl;
+        cout << "Price: " << r.price << endl;
+        cout << "Cuisine: " << r.cuisine << endl;
+        cout << "Stars: " << r.starCount << endl;
+        cout << "------------------------" << endl;
+    }
+
+    return 0;
 }
