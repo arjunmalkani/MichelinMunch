4f6bb88952549ab99eb3415e75a571c2dfdc5470
frozehu <166183471+frozehu@users.noreply.github.com>
2025-04-19 13:04:29 -0400

    created an updatemaps function to create and update the maps

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 #include "../include/DatasetLoader.h"
+#include "HashMapChaining.h";
+#include "HashMapOpen.h"
 
+void updateMaps(vector<Restaurant> &restaurants,
+    SeparateChaining &cityMapSc, SeparateChaining &starMapSc, SeparateChaining &moneyMapSc) {
+    for (Restaurant &restaurant : restaurants) {
+        string name = restaurant.name;
+        cityMapSc.insert(name, restaurant.location);
+        starMapSc.insert(name, restaurant.starCount);
+        moneyMapSc.insert(name, restaurant.price);
+    }
+}
 int main() {
     // testing data parsing
     string filename = "../data/michelin_my_maps.csv";
     vector<Restaurant> restaurants = loadDataset(filename);
+    SeparateChaining cityMapSc, starMapSc, moneyMapSc;
+    updateMaps(restaurants, cityMapSc, starMapSc, moneyMapSc);
+
+    //testing maps
+    string restaurantName = "Le Bernardin";
+    cout << "City: " << cityMapSc.search(restaurantName) << endl;
+    cout << "Stars: " << starMapSc.search(restaurantName) << endl;
+    cout << "Price: " << moneyMapSc.search(restaurantName) << endl;
 
     cout << "Total restaurants parsed: " << restaurants.size() << endl;
 
