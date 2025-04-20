3f05d277b6f61e27b5622202fb0edf5ce660d9f2
Anthony Perez <perezanthony1@ufl.edu>
2025-04-20 02:50:01 -0400

    User can input desired restaurants and also added the inclusion of michelin selected restaurants (not starred but still recommended or something)

--- src/main.cpp
+++ src/main.cpp
@@ -119,10 +119,46 @@ void mapInsertionsOA(vector<Restaurant> &restaurants, OpenAddressHashMap<string,
 
 
 int main() {
-    //testing maps
-    string city = "Miami, USA";
-    string stars = "1 Star";
-    string price = "$$$$";
+    string city;
+    string stars;
+    string price;
+
+    cout << "Welcome To MichelinMunch!\n"
+    "We are here to satisfy your cravings of meals from the Michelin Guide's list of restaurants!\n"
+    "Just follow the next 3 instructions to find restaurants which suit YOUR delicious dining desires!\n"
+    "First, please input a city followed by a comma and its country of origin. Ex: Miami, USA\n";
+    // user puts city
+    getline(cin, city);
+
+    cout << "Next, please input the amount of stars you intend to dine at (1-3)." << endl;
+    // user puts stars
+    getline(cin, stars);
+    // allows user to enter a number versus "1 Star"
+    if(stars == "1") {
+        stars += " Star";
+    } else if (stars == "2" || stars == "3") {
+        stars += " Stars";
+    } else {
+        cout << "You did not enter a star count from 1-3 so there is no such restaurant.\n"
+                "Would you like to see recommended selected restaurants from the Michelin Guide that fit your criteria instead?\n"
+                "Please enter Y or N\n";
+        string selected;
+        getline(cin, selected);
+
+        if (selected == "Y" || selected == "y") {
+            stars = "Selected Restaurants";
+
+        } else {
+            return 1;
+        }
+    }
+
+
+    cout << "Lastly, please input the price range from you intend to dine at. Ex: $-$$$$\n"
+            "PLEASE NOTE: The type of currency MUST match the currency of the country you plan to dine in!" << endl;
+    // user enters price
+    getline(cin, price);
+
 
     // testing data parsing
     string filename = "../data/michelin_my_maps.csv";
